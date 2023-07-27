f267a652fe64e86b1ffad7eb22f7d424f8db134f
djneskovic <djneskovic@gmail.com>
2023-07-27 11:28:56 +0200

    Nav finish

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,7 +1,15 @@
 <template>
-	<div>sas</div>
+	<header-navigation></header-navigation>
+	<hero-page></hero-page>
 </template>
 
 <script>
-export default {};
+import HeaderNavigation from "./components/HeaderNavigation.vue";
+import HeroPage from "./pages/HeroPage.vue";
+export default {
+	components: {
+		HeaderNavigation,
+		HeroPage,
+	},
+};
 </script>
---
+++ src/js/components/HeaderNavigation.vue
@@ -0,0 +1,69 @@
+<template>
+	<nav id="nav">
+		<div class="container">
+			<div class="nav-bar flex items-center justify-between relative">
+				<div class="nav-logo">
+					<img
+						src="../../../public/images/logo-black.png"
+						alt=""
+					/>
+				</div>
+
+				<div class="nav-list">
+					<ul
+						class="list-items"
+						:class="{ 'active-menu': isActive }"
+					>
+						<li class="list-item"><a href="#">Home</a></li>
+						<li class="list-item"><a href="#">About</a></li>
+						<li class="list-item">
+							<a href="#">Projects</a>
+						</li>
+						<li class="list-item"><a href="#">Contact</a></li>
+					</ul>
+				</div>
+
+				<div class="menu flex items-center justify-center">
+					<div class="hamburger lg:hidden">
+						<button
+							class="hamburger hamburger--elastic"
+							:class="{ 'is-active': isActive }"
+							type="button"
+							@click="toggle"
+						>
+							<span class="hamburger-box">
+								<span class="hamburger-inner"></span>
+							</span>
+						</button>
+					</div>
+					<div class="hire-me hidden md:block">
+						<button-primary class="hire"
+							>Hire Me</button-primary
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+import ButtonPrimary from "../components/ButtonPrimary.vue";
+export default {
+	components: {
+		ButtonPrimary,
+	},
+
+	data() {
+		return {
+			isActive: false,
+		};
+	},
+
+	methods: {
+		toggle() {
+			this.isActive = !this.isActive;
+		},
+	},
+};
+</script>
