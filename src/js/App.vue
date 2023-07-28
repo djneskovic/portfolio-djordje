febfc3b349b6a9ec988cbb87e6145e6fca9ac263
djneskovic <djneskovic@gmail.com>
2023-07-28 11:56:06 +0200

    Started About page

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,15 +1,18 @@
 <template>
 	<header-navigation></header-navigation>
 	<hero-page></hero-page>
+	<about-page></about-page>
 </template>
 
 <script>
 import HeaderNavigation from "./components/HeaderNavigation.vue";
 import HeroPage from "./pages/HeroPage.vue";
+import AboutPage from "./pages/AboutPage.vue";
 export default {
 	components: {
 		HeaderNavigation,
 		HeroPage,
+		AboutPage,
 	},
 };
 </script>
---
+++ src/js/pages/AboutPage.vue
@@ -0,0 +1,49 @@
+<template>
+	<main id="about">
+		<div class="container">
+			<section class="about-me">
+				<h2 class="about-title text-center">About Me</h2>
+
+				<div
+					class="about-section flex flex-col md:flex-row items-center justify-center md:justify-evenly xl:justify-between"
+				>
+					<div class="about-context">
+						<h3>
+							Junior Vue Developer
+							<span>Seeking</span> Job Opportunities
+						</h3>
+
+						<p class="about-text">
+							I am a junior Vue Developer actively seeking
+							employment opportunities. With a strong
+							background in Vue development, I possess the
+							technical expertise and experience necessary
+							to contribute to any development team. My
+							proficiency in Vue.js allows me to create
+							efficient and scalable web applications. I am
+							highly motivated, detail-oriented, and
+							committed to delivering high-quality code.
+						</p>
+						<button-primary>Contact Me</button-primary>
+					</div>
+
+					<div class="about-image">
+						<img
+							src="../../../public/images/guy.png"
+							alt=""
+						/>
+					</div>
+				</div>
+			</section>
+		</div>
+	</main>
+</template>
+
+<script>
+import ButtonPrimary from "../components/ButtonPrimary.vue";
+export default {
+	components: {
+		ButtonPrimary,
+	},
+};
+</script>
